cb0a73273080c9b8b9b4abaf89dff187f6c1c342
Zi-on <zioniflores12@yahoo.com>
2021-06-05 18:15:15 -0500

    added icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,28 +57,37 @@ searchBtn.on("click", function (event) {
         .then(function(data) {
             console.log(data)
             console.log(data.current.temp)
+
+            var currentIcon = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png"
+
+
             
-            document.getElementById("currentIcon").innerHTML = data.current.weather[0].icon
-            document.getElementById("currentTemp").innerHTML = "Temp: "+data.current.temp
-            document.getElementById("currentWind").innerHTML = "Wind: "+data.current.wind_speed
-            document.getElementById("currentHumidity").innerHTML = "Humidity: "+data.current.humidity
+            $("#icon1").attr("src", currentIcon)
+            $("#icon2").attr("src", "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + "@2x.png")
+            $("#icon3").attr("src", "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png")
+            $("#icon4").attr("src", "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + "@2x.png")
+            $("#icon5").attr("src", "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + "@2x.png")
+            $("#icon6").attr("src", "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + "@2x.png")
+            document.getElementById("currentTemp").innerHTML = "Temp: "+data.current.temp+" "+"°F"
+            document.getElementById("currentWind").innerHTML = "Wind: "+data.current.wind_speed+" "+"Mph"
+            document.getElementById("currentHumidity").innerHTML = "Humidity: "+data.current.humidity+"%"
             document.getElementById("currentUvi").innerHTML = "UV index: "+data.current.uvi
 
-            document.getElementById("day1Temp").innerHTML = "Temp: "+data.daily[0].temp.day
-            document.getElementById("day1Wind").innerHTML = "Wind: "+data.daily[0].wind_speed
-            document.getElementById("day1Humidity").innerHTML = "Humidity: "+data.daily[0].humidity
-            document.getElementById("day2Temp").innerHTML = "Temp: "+data.daily[1].temp.day
-            document.getElementById("day2Wind").innerHTML = "Wind: "+data.daily[1].wind_speed
-            document.getElementById("day2Humidity").innerHTML = "Humidity: "+data.daily[1].humidity
-            document.getElementById("day3Temp").innerHTML = "Temp: "+data.daily[2].temp.day
-            document.getElementById("day3Wind").innerHTML = "Wind: "+data.daily[2].wind_speed
-            document.getElementById("day3Humidity").innerHTML = "Humidity: "+data.daily[2].humidity
-            document.getElementById("day4Temp").innerHTML = "Temp: "+data.daily[3].temp.day
-            document.getElementById("day4Wind").innerHTML = "Wind: "+data.daily[3].wind_speed
-            document.getElementById("day4Humidity").innerHTML = "Humidity: "+data.daily[3].humidity
-            document.getElementById("day5Temp").innerHTML = "Temp: "+data.daily[4].temp.day
-            document.getElementById("day5Wind").innerHTML = "Wind: "+data.daily[4].wind_speed
-            document.getElementById("day5Humidity").innerHTML = "Humidity: "+data.daily[4].humidity
+            document.getElementById("day1Temp").innerHTML = "Temp: "+data.daily[0].temp.day+" "+"°F"
+            document.getElementById("day1Wind").innerHTML = "Wind: "+data.daily[0].wind_speed+" "+"Mph"
+            document.getElementById("day1Humidity").innerHTML = "Humidity: "+data.daily[0].humidity+"%"
+            document.getElementById("day2Temp").innerHTML = "Temp: "+data.daily[1].temp.day+" "+"°F"
+            document.getElementById("day2Wind").innerHTML = "Wind: "+data.daily[1].wind_speed+" "+"Mph"
+            document.getElementById("day2Humidity").innerHTML = "Humidity: "+data.daily[1].humidity+"%"
+            document.getElementById("day3Temp").innerHTML = "Temp: "+data.daily[2].temp.day+" "+"°F"
+            document.getElementById("day3Wind").innerHTML = "Wind: "+data.daily[2].wind_speed+" "+"Mph"
+            document.getElementById("day3Humidity").innerHTML = "Humidity: "+data.daily[2].humidity+"%"
+            document.getElementById("day4Temp").innerHTML = "Temp: "+data.daily[3].temp.day+" "+"°F"
+            document.getElementById("day4Wind").innerHTML = "Wind: "+data.daily[3].wind_speed+" "+"Mph"
+            document.getElementById("day4Humidity").innerHTML = "Humidity: "+data.daily[3].humidity+"%"
+            document.getElementById("day5Temp").innerHTML = "Temp: "+data.daily[4].temp.day+" "+"°F"
+            document.getElementById("day5Wind").innerHTML = "Wind: "+data.daily[4].wind_speed+" "+"Mph"
+            document.getElementById("day5Humidity").innerHTML = "Humidity: "+data.daily[4].humidity+"%"
 
             
             
