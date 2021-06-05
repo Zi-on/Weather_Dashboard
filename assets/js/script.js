6943e2afbacd78ddf15d7286dec4738a77d72a8a
Zi-on <zioniflores12@yahoo.com>
2021-06-05 12:25:23 -0500

    linked all my api data to its appropiate location on my html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,19 @@
 var searchArea = $("#searchArea");
 var searchBtn = $('#search');
 
+var day1 = moment().add(1, 'days').format('ddd')
+var day2 = moment().add(2, 'days').format('ddd')
+var day3 = moment().add(3, 'days').format('ddd')
+var day4 = moment().add(4, 'days').format('ddd')
+var day5 = moment().add(5, 'days').format('ddd')
+
+
+document.getElementById("day1").innerHTML = day1
+document.getElementById("day2").innerHTML = day2
+document.getElementById("day3").innerHTML = day3
+document.getElementById("day4").innerHTML = day4
+document.getElementById("day5").innerHTML = day5
+
 
 searchBtn.on("click", function (event) {
     
@@ -48,6 +61,29 @@ searchBtn.on("click", function (event) {
             localStorage.setItem("current wind speed", data.current.wind_speed)
             localStorage.setItem("current humidity", data.current.humidity)
             localStorage.setItem("UV index", data.current.uvi)
+
+            document.getElementById("currentTemp").innerHTML = "Temp: "+data.current.temp
+            document.getElementById("currentWind").innerHTML = "Wind: "+data.current.wind_speed
+            document.getElementById("currentHumidity").innerHTML = "Humidity: "+data.current.humidity
+            document.getElementById("currentUvi").innerHTML = "UV index: "+data.current.uvi
+
+            document.getElementById("day1Temp").innerHTML = "Temp: "+data.daily[0].temp.day
+            document.getElementById("day1Wind").innerHTML = "Wind: "+data.daily[0].wind_speed
+            document.getElementById("day1Humidity").innerHTML = "Humidity: "+data.daily[0].humidity
+            document.getElementById("day2Temp").innerHTML = "Temp: "+data.daily[1].temp.day
+            document.getElementById("day2Wind").innerHTML = "Wind: "+data.daily[1].wind_speed
+            document.getElementById("day2Humidity").innerHTML = "Humidity: "+data.daily[1].humidity
+            document.getElementById("day3Temp").innerHTML = "Temp: "+data.daily[2].temp.day
+            document.getElementById("day3Wind").innerHTML = "Wind: "+data.daily[2].wind_speed
+            document.getElementById("day3Humidity").innerHTML = "Humidity: "+data.daily[2].humidity
+            document.getElementById("day4Temp").innerHTML = "Temp: "+data.daily[3].temp.day
+            document.getElementById("day4Wind").innerHTML = "Wind: "+data.daily[3].wind_speed
+            document.getElementById("day4Humidity").innerHTML = "Humidity: "+data.daily[3].humidity
+            document.getElementById("day5Temp").innerHTML = "Temp: "+data.daily[4].temp.day
+            document.getElementById("day5Wind").innerHTML = "Wind: "+data.daily[4].wind_speed
+            document.getElementById("day5Humidity").innerHTML = "Humidity: "+data.daily[4].humidity
+
+            
             
         })
     })
