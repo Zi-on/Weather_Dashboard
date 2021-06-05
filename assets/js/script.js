397fb8483a8569c762c36aa084301dd690cb6241
Zi-on <zioniflores12@yahoo.com>
2021-06-05 01:14:21 -0500

    called a api to get longitude and latitude from a city name. then it puts those values into an api that gets the weather details for that location. i add these current attributes to my local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,47 @@
 //         console.log(response.json());
 //     })
 
-var searchArea = $(".searchArea");
-var searchBtn = $('.search');
+var searchArea = $("#searchArea");
+var searchBtn = $('#search');
+
 
 searchBtn.on("click", function (event) {
-    var clickBtn = event.target;
-    var city = $(clickBtn).siblings()[1].value;
+    
+    var city = searchArea.val().trim();
+    var cityApi = "http://api.openweathermap.org/geo/1.0/direct?q="+city+",tx,US&limit=5&appid=d727e0c365295376fcdf1f7932a17a35"
   
+    fetch(cityApi, {
+    
+})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        console.log("Latitude: " + data[0].lat)
+        console.log("Longitude: " + data[0].lon)
+        localStorage.setItem("name", data[0].name)
+        var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+data[0].lat+"&lon="+data[0].lon+"&exclude={part}&units=imperial&appid=d727e0c365295376fcdf1f7932a17a35"
+        document.getElementById("cityName").innerHTML = data[0].name
 
-    console.log(city)
+        fetch(weatherApiUrl, {
+
+        })
+    
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            console.log(data.current.temp)
+            localStorage.setItem("current temp", data.current.temp)
+            localStorage.setItem("current wind speed", data.current.wind_speed)
+            localStorage.setItem("current humidity", data.current.humidity)
+            localStorage.setItem("UV index", data.current.uvi)
+            
+        })
+    })
+   
 })
    
 
